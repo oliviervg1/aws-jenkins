0f871df0bface993fb7b207b11f521118fa7ca51
Olivier Van Goethem <olivier.van-goethem@cloudreach.co.uk>
2017-04-20 09:24:22 +0200

    Fetch Jenkins gpg key from https site

--- userdata/jenkins.sh
+++ userdata/jenkins.sh
@@ -14,8 +14,8 @@ yum install -y java-1.8.0-openjdk
 yum remove -y java-1.7.0-openjdk
 
 # Install Jenkins
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins-ci.org.key
 yum install -y jenkins
 
 # Restore possible backup
