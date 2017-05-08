969e0999a1b22eb94c96e8d61c61dbf39c8de59e
Olivier Van Goethem <olivier.van-goethem@cloudreach.co.uk>
2017-05-08 14:32:16 +0200

    Enable hourly backups via cron

--- userdata/jenkins.sh
+++ userdata/jenkins.sh
@@ -3,7 +3,7 @@
 # Config
 export AWS_DEFAULT_REGION=eu-west-2
 JENKINS_HOME=/var/lib/jenkins
-S3_BACKUP=s3://oliviervg-jenkins/
+S3_BACKUP_BUCKET=s3://oliviervg-jenkins/
 
 # Update system
 yum clean all
@@ -22,14 +22,46 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
 
 # Restore possible backup
-if [[ $S3_BACKUP == */ ]]; then
-    S3_BACKUP=$S3_BACKUP`aws s3 ls $S3_BACKUP|tail -1|awk '{print $NF}'`
+if [[ $S3_BACKUP_BUCKET == */ ]]; then
+    S3_BACKUP_BUCKET=$S3_BACKUP_BUCKET`aws s3 ls $S3_BACKUP_BUCKET|tail -1|awk '{print $NF}'`
 fi
-LOCAL_BACKUP=/tmp/`basename $S3_BACKUP`
-aws s3 cp $S3_BACKUP $LOCAL_BACKUP
+LOCAL_BACKUP=/tmp/`basename $S3_BACKUP_BUCKET`
+aws s3 cp $S3_BACKUP_BUCKET $LOCAL_BACKUP
 mkdir -p $JENKINS_HOME
 tar zxf $LOCAL_BACKUP -C $JENKINS_HOME
 rm -f $LOCAL_BACKUP
+
+# Enable backup script
+cat >/usr/local/bin/jenkins_backup <<EOL
+#!/bin/bash -e
+
+S3_BACKUP_BUCKET=$S3_BACKUP_BUCKET
+S3_BACKUP_FILENAME=\`date +%Y-%m-%d:%H:%M:%S\`.zip
+LOCAL_BACKUP=/tmp/\`basename \$S3_BACKUP_FILENAME\`
+
+tar -C $JENKINS_HOME -zcf \$LOCAL_BACKUP . \
+    --exclude "config-history/" \
+    --exclude "config-history/*" \
+    --exclude "jobs/*/workspace*" \
+    --exclude "jobs/*/builds/*/archive" \
+    --exclude "plugins/*/*" \
+    --exclude "plugins/*.bak" \
+    --exclude "war" \
+    --exclude "cache" \
+    --exclude ".ssh/" \
+    --exclude ".ssh/*"
+
+aws --region eu-west-2 s3 cp \$LOCAL_BACKUP \$S3_BACKUP_BUCKET\$S3_BACKUP_FILENAME
+rm -rf \$LOCAL_BACKUP
+EOL
+chown root:root /usr/local/bin/jenkins_backup
+chmod 755 /usr/local/bin/jenkins_backup
+
+# Schedule hourly backups
+echo "0 * * * * root /usr/local/bin/jenkins_backup >> /var/log/jenkins_backup.log 2>&1" > /etc/cron.d/jenkins
+chown root:root /etc/cron.d/jenkins
+chmod 644 /etc/cron.d/jenkins
+service crond restart
 
 # Start Jenkins
 service jenkins start
